8bb9a72b8541ee032aaec5e7d56fe499e4695e06
ваня волошин <vanek2004@icloud.com>
2022-06-23 20:05:43 +0300

    2

--- Program.cs
+++ Program.cs
@@ -7,14 +7,31 @@ namespace Testing
     {
         static void Main(string[] args)
         { 
-            string templateFile = @"D:\Project via\Testing\Шаблон.docx";
+            string templateFile = @"D:\Project via\Demo\Sample.pdf";
+            var wordApp = new Word.Application();
+            var wordDoc = wordApp.Documents.Add(templateFile);
+            
 
             Console.WriteLine("name:");
             string Name = Console.ReadLine();
-            Console.WriteLine($"Имя - {Name}");
 
-            var wordApp = new Word.Application();// создаем новый экземпляр ворда
-            //Функция для замены наших меток
+            Console.WriteLine("Age:");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine("Profesion:");
+            string Prof = Console.ReadLine();
+
+            Console.WriteLine("Where you study:");
+            string Stud = Console.ReadLine();
+
+            Console.WriteLine("Info by you:");
+            string Info = Console.ReadLine();
+
+            Console.WriteLine($"Name - {Name}, Age - {Age}, Profetsion - {Prof}, Study - {Stud}, Info by you - {Info},");
+
+            Console.WriteLine("What format to save");
+            string SaveFormat = Console.ReadLine();
+            
             void ReplaceStub(string stubToReplace, string text, Word.Document worldDocument)
             {
                 var range = worldDocument.Content;
@@ -25,16 +42,37 @@ namespace Testing
 
             try
             {
-                var wordDoc = wordApp.Documents.Add(templateFile);
-
-                ReplaceStub("{fio}", Name, wordDoc);//Заменяем метку на данные из формы
-                ///Может быть много таких меток
-                wordDoc.SaveAs(@"D:\Project via\Testing\Резюме" + "_" + Name);
+                ReplaceStub("{fio}", Name, wordDoc);
+                ReplaceStub("{age}", Age, wordDoc);
+                ReplaceStub("{prof}", Prof, wordDoc);
+                ReplaceStub("{study}", Stud, wordDoc);
+                ReplaceStub("{info}", Info, wordDoc);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
+
+            switch (SaveFormat)
+            {
+                case "dox":
+                    wordDoc.SaveAs(@"D:\Project via\Demo\Резюме" + "_" + Name);
+                    wordDoc.Close();
+                    break;
+                case "pdf":
+                    wordDoc.SaveAs(@"D:\Project via\Demo\Резюме" + "_" + Name, Word.WdSaveFormat.wdFormatPDF);
+                    wordDoc.Close();
+                    break;
+                case "txt":
+                    wordDoc.SaveAs(@"D:\Project via\Demo\Резюме" + "_" + Name, Word.WdSaveFormat.wdFormatText);
+                    wordDoc.Close();
+                    break;
+                default:
+                    Console.WriteLine("this format is not available");
+                    break;
+            }
+
         }
+
     }
 }
